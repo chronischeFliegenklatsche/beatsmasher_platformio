4ad47b617bd3c93c070a82ff41937cb7d1249c50
chronischeFliegenklatsche <nanuyalrif@gmail.com>
2024-06-29 12:44:06 +0200

    von sem stunde

--- lib/beatsmasher/src/Prefabs/FallingBlock.cpp
+++ lib/beatsmasher/src/Prefabs/FallingBlock.cpp
@@ -2,6 +2,7 @@
 #define FALLING_BLOCK_CPP
 
 #include <smash.h>
+#include "Scripts/Fall.cpp"
 
 class FallingBlock : public smash::GameObject {
 
@@ -9,6 +10,7 @@ class FallingBlock : public smash::GameObject {
         FallingBlock(float x, float y) : GameObject() {
             addComponent(std::make_shared<smash::Transform>(smash::Vector2(0, 0)));
             addComponent(std::make_shared<smash::RectangleRenderer>(smash::Vector2(4), smash::Color(0, 0, 255, 255)));
+            addComponent(std::make_shared<Fall>(1.0f));
         }
 };
 
